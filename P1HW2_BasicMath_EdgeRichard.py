1f5425d8f8b6613cb2e10bfc2d6b5b058af955e3
edger1212 <85772193+edger1212@users.noreply.github.com>
2021-06-13 15:03:02 -0400

    Add files via upload
    Fifth Submission

---
+++ P1HW2_BasicMath_EdgeRichard.py
@@ -0,0 +1,18 @@
+# P1HW2_Basic_Math
+# 12 June 2021
+# CTI-110 P1HW2 - Basic Math
+# Richard Edge
+# 
+first_no= int(input("Please Enter First Number: "))
+print()
+second_no= int(input("Please Enter Second Number: "))
+print()
+
+print(first_no)
+print(second_no)
+
+sum_of_nos= first_no+second_no 
+product_of_nos= first_no*second_no
+
+print(sum_of_nos)
+print(product_of_nos)
